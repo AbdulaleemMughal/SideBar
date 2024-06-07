aa357ef23008d2d05a4910432830f3cbd89d2740
Abdulaleem Mughal <mughalabdulaleem8@gmail.com>
2024-06-06 17:20:09 -0900

    completed

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import "./App.css";
+import DashBoard from "./pages/DashBoard";
+import Analytics from "./pages/Analytics";
+import FileManager from "./pages/FileManager";
+import Messages from "./pages/Messages";
+import Order from "./pages/Order";
+import Saved from "./pages/Saved";
+import Setting from "./pages/Setting";
+import User from "./pages/User";
+import SideBar from "./components/SideBar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <SideBar>
+      <Routes>
+        <Route path="/" element={<DashBoard />} />
+        <Route path="/analytics" element={<Analytics />} />
+        <Route path="/filemanager" element={<FileManager />} />
+        <Route path="/message" element={<Messages />} />
+        <Route path="/order" element={<Order />} />
+        <Route path="/saved" element={<Saved />} />
+        <Route path="/setting" element={<Setting />} />
+        <Route path="/user" element={<User />} />
+      </Routes>
+      </SideBar>
+    </Router>
   );
 }
 
---
+++ src/pages/FileManager.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const FileManager = () => {
+  return (
+    <div>
+      <h1>u</h1>
+    </div>
+  )
+}
+
+export default FileManager
